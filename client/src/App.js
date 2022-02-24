b29cadf0bb58ab6b97262f3c8e74335fc9587083
Stephen Schrauger <git@schrauger.com>
2022-02-24 14:59:18 -0500

    can't figure out why bootstrap isn't loading I can work on making it pretty later if I have time

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-//import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-      // <div className="flex-column justify-center align-center min-100-vh bg-primary">
         <Home />
-      // { </div> }
   );
 }
 
